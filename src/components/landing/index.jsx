c8a72b90b8d116ab02465b360376da54a2dbe6a5
Saad Sleem <saadsleem80@gmail.com>
2025-04-20 18:10:29 +0200

    fix-style

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -7,7 +7,7 @@ import resume from '@/assets/Saad MOHAMED SLEEM CV.pdf';
 
 function Landing() {
   return (
-    <section id='home' className='py-20' style={{height: 'calc(100vh - 80px)'}}>
+    <section id='home' className='py-20'>
         <Container >
             <div className='relative h-full'>
                 <div className='absolute bottom-0 right-0 opacity-10'>
@@ -19,11 +19,11 @@ function Landing() {
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-10 h-full z-10'>
                     <div className='flex flex-col gap-7 my-auto'>
                         <div>
-                            <h1 className='text-5xl font-bold'>Hello, I'm <span className='text-green-500'>Saad Sleem</span></h1>
-                            <p className='text-gray-400 text-xl mt-4'>Full Stack Developer</p>
+                            <h1 className='text-3xl md:text-5xl font-bold'>Hello, I'm <span className='text-green-500'>Saad Sleem</span></h1>
+                            <p className='text-gray-400 text-md md:text-xl mt-4'>Full Stack Developer</p>
                         </div>
                         <div className='flex flex-col gap-4'>
-                            <p className='text-gray-400 text-foreground text-xl'>I build robust and scalable web applications using Laravel, React, Vue.js, and various other modern technologies. With expertise in both frontend and backend development, I create seamless digital experiences that solve real-world problems.</p>
+                            <p className='text-gray-400 text-foreground text-md md:text-xl'>I build robust and scalable web applications using Laravel, React, Vue.js, and various other modern technologies. With expertise in both frontend and backend development, I create seamless digital experiences that solve real-world problems.</p>
                             <div className='flex align-center gap-4'>
                                 <a href="#projects">
                                     <Button href="#projects" className='bg-green-400 text-white hover:bg-green-500 py-5 px-10'>
